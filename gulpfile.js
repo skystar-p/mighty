6bd2e64a071198692f7a176e43c414ccac775fd2
VBChunguk <chwo9843@gmail.com>
2017-08-05 03:14:18 +0900

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ let typescript = require('gulp-typescript');
 const tsProject = typescript.createProject('tsconfig.json');
 
 gulp.task('build', [], function () {
-    gulp.pipe(tsProject())
-    .pipe(gulp.dest('dist/'));
+    return tsProject.src()
+    .pipe(tsProject())
+    .js.pipe(gulp.dest('dist/'));
 });
