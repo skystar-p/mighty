5f2d2aeb0b98761e52e29e15643bca973022a860
VBChunguk <chwo9843@gmail.com>
2017-08-05 02:43:59 +0900

    Use ES6 to build

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 let gulp = require('gulp');
 let typescript = require('gulp-typescript');
+const tsProject = typescript.createProject('tsconfig.json');
 
 gulp.task('build', [], function () {
-    gulp.src('src/**/*.ts')
-    .pipe(typescript())
+    gulp.pipe(tsProject())
     .pipe(gulp.dest('dist/'));
 });
